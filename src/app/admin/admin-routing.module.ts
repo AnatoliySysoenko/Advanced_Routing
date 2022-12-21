b2cdf0275395b9de03ad2fc6b4a1f3a6d5a11800
anatoliySysoienko <syzzzam@gmail.com>
2022-12-21 14:37:56 +0200

    admin panel, manage-phrases, manage-users

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHostComponent } from './components/admin-host/admin-host.component';
+import { ManagePhrasesComponent } from './components/manage-phrases/manage-phrases.component';
+import { ManageUsersComponent } from './components/manage-users/manage-users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminHostComponent,
+    children: [
+      {path: 'phrases', component: ManagePhrasesComponent},
+      {path: 'users', component: ManageUsersComponent},
+      {path: '', redirectTo: 'phrases', pathMatch: 'full'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {
+}
